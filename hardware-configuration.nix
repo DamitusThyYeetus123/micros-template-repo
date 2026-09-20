3cdbe4348a4cf39f6d4ac587b4bece7f17860458
Damiii <108782125+DamitusThyYeetus123@users.noreply.github.com>
2026-09-20 21:31:50 +1000

    Update hardware-configuration.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,42 +1,13 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
-  # The initrd has to contain any module that might be necessary for
-  # supporting the most important parts of HW like drives.
+{...}: {
   boot.initrd.kernelModules = [
-    "3w-9xxx"
-    "3w-xxxx"
+    # SATA/PATA support.
     "ahci"
-    "aic79xx"
-    "aic7xxx"
-    "arcmsr"
+
     "ata_piix"
-    "cdrom"
-    "ch"
-    "dm_mod"
-    "ext2"
-    "ext4"
-    "g_mass_storage"
-    "hid"
-    "hpsa"
-    "i8042"
-    "iso9660"
-    "isofs"
-    "libcomposite"
-    "loop"
-    "mc"
-    "mmc_block"
-    "mptspi"
-    "nvme"
-    "ohci-hcd"
-    "ohci1394"
-    "pata_marvell"
-    "pcips2"
+
     "sata_inic162x"
-    "sata_mv"
     "sata_nv"
+    "sata_mv"
     "sata_promise"
     "sata_qstor"
     "sata_sil"
@@ -47,33 +18,96 @@
     "sata_uli"
     "sata_via"
     "sata_vsc"
-    "sbp2"
+
+    "pata_ali"
+    "pata_amd"
+    "pata_artop"
+    "pata_atiixp"
+    "pata_efar"
+    "pata_hpt366"
+    "pata_hpt37x"
+    "pata_hpt3x2n"
+    "pata_hpt3x3"
+    "pata_it8213"
+    "pata_it821x"
+    "pata_jmicron"
+    "pata_marvell"
+    "pata_mpiix"
+    "pata_netcell"
+    "pata_ns87410"
+    "pata_oldpiix"
+    "pata_pcmcia"
+    "pata_pdc2027x"
+    "pata_qdi"
+    "pata_rz1000"
+    "pata_serverworks"
+    "pata_sil680"
+    "pata_sis"
+    "pata_sl82c105"
+    "pata_triflex"
+    "pata_via"
+    "pata_winbond"
+
+    # SCSI support (incomplete).
+    "3w-9xxx"
+    "3w-xxxx"
+    "aic79xx"
+    "aic7xxx"
+    "arcmsr"
+    "hpsa"
+
+    # USB support, especially for booting from USB CD-ROM
+    # drives.
+    "uas"
+    "uhci-hcd"
+    "ohci-hcd"
+    "usb-storage"
+    "hid"
+    "cdrom"
+    "sr_mod"
+    "mc"
+    "iso9660"
+    "isofs"
+    "sg"
+    "st"
+    "ch"
     "scsi_common"
     "scsi_mod"
-    "sd_mod"
-    "sdhci_pci"
-    "sg"
-    "squashfs"
-    "sr_mod"
-    "st"
-    "tun"
-    "uas"
     "ufshcd-core"
     "ufshcd-pci"
     "ufshcd-pltfrm"
-    "uhci-hcd"
-    "usb-storage"
     "usb_f_mass_storage"
-    "virtio"
-    "virtio_balloon"
-    "virtio_blk"
-    "virtio_console"
-    "virtio_mmio"
+    "g_mass_storage"
+    "libcomposite"
+    "sd_mod"
+    # SD cards.
+    "sdhci_pci"
+
+    # NVMe drives
+    "nvme"
+
+    # Firewire support.  Not tested.
+    "ohci1394"
+    "sbp2"
+
+    # Virtio (QEMU, KVM etc.) support.
     "virtio_net"
     "virtio_pci"
+    "virtio_mmio"
+    "virtio_blk"
+    "virtio"
     "virtio_scsi"
-    "xhci_hcd"
-    "xhci_pci"
+    "virtio_balloon"
+    "virtio_console"
+    "af_packet"
+    # VMware support.
+    "mptspi"
+    "vmxnet3"
+    "vsock"
+
+    "i8042"
+    "atkbd"
+    "psmouse"
   ];
   fileSystems."/" = {
     device = "tmpfs";
@@ -92,4 +126,9 @@
     options = ["ro"];
     neededForBoot = true;
   };
+  networking.interfaces = [
+    {
+      name = "eth0";
+    }
+  ];
 }
